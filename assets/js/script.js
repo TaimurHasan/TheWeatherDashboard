cf4f2ae346f891f67c30a53e5b8611948a17e0d4
TaimurHasan <taimurhasan11@gmail.com>
2022-03-25 17:36:29 -0400

    set up js to pull important city country and weather information from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,50 @@
-var formEl = document.querySelector("#search-form")
-var apiKey = "6d797a2c3d8a01c9b09004f544fc4746"
+var formEl = document.querySelector("#search-form");
+var apiKey = "6d797a2c3d8a01c9b09004f544fc4746";
 
+// handle form input and send through api functions
 var formHandler = function (event) {
     event.preventDefault();
-    var cityInput = document.querySelector("input[id='city']").value;
-    getWeather(cityInput);
+    var cityInput = document.querySelector("input[id='city']").value.split(" ").join("");
+    getCityDetails(cityInput);
 }
 
-var getWeather = function (city) {
-    apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=";
-    console.log(apiUrl);
+// call on api specifically to get latitude and longitude from searched city name + save city name and country
+var getCityDetails = function (city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    
+    fetch(apiUrl)
+        .then(function(response) {
+            response.json()
+            .then(function(data) {
+                var lon = data.coord.lon;
+                var lat = data.coord.lat;
+                var cityName = data.name;
+                var country = data.sys.country;
+                getWeather(lat, lon, cityName, country);
+            })
+        })
 }
 
+//call on onecall api to get weather from latitude and longitude
+var getWeather = function (lat, lon, cityName, country) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=" + apiKey;
+    
+    fetch(apiUrl)
+        .then(function(response) {
+            response.json()
+            .then(function(data){
+                displayCurrentWeather(data, cityName, country);
+            })
+        })
+}
+
+// function to display weather fetched from api onto display
+var displayCurrentWeather = function(data, cityName, country) {
+    console.log(cityName);
+    console.log(country);
+    console.log(data);
+
+    
+}
 
 formEl.addEventListener("submit", formHandler)
