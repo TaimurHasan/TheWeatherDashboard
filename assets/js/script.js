82c02de8499250c2b636de43bcaa9df5b9ce7c7a
TaimurHasan <taimurhasan11@gmail.com>
2022-03-26 15:59:10 -0400

    pull current weather data and append main metrics to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var formEl = document.querySelector("#search-form");
 var apiKey = "6d797a2c3d8a01c9b09004f544fc4746";
+var searchHistory = [];
+var weatherDash = document.querySelector("#weather");
 
 // handle form input and send through api functions
 var formHandler = function (event) {
@@ -27,24 +29,67 @@ var getCityDetails = function (city) {
 
 //call on onecall api to get weather from latitude and longitude
 var getWeather = function (lat, lon, cityName, country) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + apiKey;
     
     fetch(apiUrl)
         .then(function(response) {
             response.json()
             .then(function(data){
-                displayCurrentWeather(data, cityName, country);
+                displayCurrent(data, cityName, country);
             })
         })
 }
 
 // function to display weather fetched from api onto display
-var displayCurrentWeather = function(data, cityName, country) {
-    console.log(cityName);
-    console.log(country);
+var displayCurrent = function(data, cityName, country) {
     console.log(data);
 
+    // get full date
+    var theTime = new Date(data.current.dt * 1000);
+    var fullDate = getDate(theTime);
+
+
+    var weatherDivEl = document.createElement("div");
+    weatherDivEl.className = "col-8 current-weather";
+    weatherDivEl.setAttribute("id", "current-weather");
+
+    var weatherHeadEl = document.createElement("h1");
+    weatherHeadEl.textContent = cityName + ", " + country + " - " + fullDate;
+
+    weatherDivEl.appendChild(weatherHeadEl);
+    weatherDash.appendChild(weatherDivEl);
+    displayWeather(data, weatherDivEl);
+};
+
+// function to take the time and turn it into readable date
+var getDate = function(theTime) {
+    month = theTime.getMonth();
+    date = theTime.getDate();
+    year = theTime.getFullYear();
+
+    var fullDate = month + "/" + date + "/" + year;
+    return fullDate;
+}
+
+var displayWeather = function (data, selected) {
+    console.log(data.current.temp);
     
+    // required metrics
+    var temp = "Temp: " + Math.round(data.current.temp) + '\xB0';
+    var wind = "Wind: " + data.current.wind_speed + " MPH";
+    var metrics = [temp, wind];
+    
+    var listEl = document.createElement("ul");
+    
+    for (var i = 0; i < metrics.length; i++) {
+        var metric = metrics[i]; 
+        var listItemEl = document.createElement("li");
+        listItemEl.textContent = metric;
+        listEl.appendChild(listItemEl);
+    };
+    
+    selected.appendChild(listEl);
+
 }
 
 formEl.addEventListener("submit", formHandler)
